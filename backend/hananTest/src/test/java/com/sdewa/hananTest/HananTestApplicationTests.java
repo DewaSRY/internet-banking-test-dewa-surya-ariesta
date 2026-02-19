e4edc34efcbe9bb9d127650d38996569a91f5882
DewaSRY <sdewa6645@gmail.com>
2026-02-19 15:04:50 +0800

    init: backend

---
+++ backend/hananTest/src/test/java/com/sdewa/hananTest/HananTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdewa.hananTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HananTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
